ed545cd7aa660c562ab543a508590e0170194434
Lorena Costa <loraphaela@gmail.com>
2021-04-07 22:25:38 +1200

    start home page styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import Home from "./pages/Home";
 import Story from "./pages/Story";
 
+import { Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Route exact path="/" component={Home} />
+      <Route path="/story" component={Story} />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,18 @@
 export default function Home(props) {
-  return <h1>HOME</h1>;
+  return (
+    <div className="container">
+      <h1 className="title">HOME</h1>
+      <div className="row">
+        <div className="col">1</div>
+        <div className="col">2</div>
+        <div className="col">3</div>
+        <div className="col">4</div>
+        <div className="col">5</div>
+        <div className="col">6</div>
+        <div className="col">7</div>
+        <div className="col">8</div>
+        <div className="col">9</div>
+      </div>
+    </div>
+  );
 }
